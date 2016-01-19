af982cfff78710c9e27af0e4a5e75b50297e3317
innlouvate <l.j.franklin85@googlemail.com>
2016-01-19 11:36:45 +0000

    finished 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,16 @@
 require 'docking_station'
 
+=begin
 describe DockingStation do
+  #subject { DockingStation.new }
+  it 'responds to the method release_bike' do
+    expect(subject).to respond_to :release_bike
+  end
+end
 
+=end
+
+describe DockingStation do
+  it {is_expected.to respond_to :release_bike}
+  
 end
