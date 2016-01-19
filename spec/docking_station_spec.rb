efc4421e031fada3cd3a64e79d6ae54bb532a66a
innlouvate <l.j.franklin85@googlemail.com>
2016-01-19 12:50:34 +0000

    finished ex 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,5 +12,17 @@ end
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
-  
+  it 'releases a bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
+
+
+=begin
+describe :release_bike do
+  it {is_expected.not_to be_empty}
+end
+
+=end
