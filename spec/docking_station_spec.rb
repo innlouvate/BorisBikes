aead9be735805db0869b3b35d810f54bf01318c1
innlouvate <l.j.franklin85@googlemail.com>
2016-01-19 15:10:33 +0000

    pre-branch commit

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,28 +1,15 @@
 require 'docking_station'
 
-=begin
 describe DockingStation do
-  #subject { DockingStation.new }
-  it 'responds to the method release_bike' do
-    expect(subject).to respond_to :release_bike
-  end
-end
-
-=end
-
-describe DockingStation do
-  it {is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to :release_bike }
   it 'releases a bike' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
 
+  it { is_expected. to respond_to :dock_bike }
+  it 'docks bike' do
+
+    expect(subject).to be_an_instance_of <Bike> 
+  end
 end
-
-
-=begin
-describe :release_bike do
-  it {is_expected.not_to be_empty}
-end
-
-=end
