b6c36f3554108c8b48f09c9e25257dc6c68a992b
itsindigo <matthewbridgesbusiness@gmail.com>
2016-01-20 15:04:02 +0000

    Added release bike exception + test, adjusted .rspec format, added describe-do nests

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -5,19 +5,13 @@ class DockingStation
 	attr_reader :bike
 
 	def release_bike
-		Bike.new
+		fail "No Bike Error" unless @bike
+		@bike
 	end
 
 	def dock(bike)
 		@bike = bike
 	end
-
 end
 
-# dock = DockingStation.new
-#
-# bike1 = dock.release_bike
-#
-# dock.bike = bike1
-#
-# dock.bike
+
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -8,14 +8,28 @@ describe DockingStation do
   	it 'expects bike to be working in release_bike' do
   		expect(subject.release_bike.working?).to be true
   	end
-
-  	it 'returns docked bikes' do
-			bike = Bike.new #basically the same as if we ran release bike first
+	
+	describe "#release_bike" do
+		it "releases a bike" do
+			bike = Bike.new
 			subject.dock(bike)
+			expect(subject.release_bike).to eq bike
+		end
+
+		it "Should raise error if bikes not available" do
+			expect { subject.release_bike }.to raise_error "No Bike Error" 
+		end 
+	end
+
+	describe "#dock_bike" do
+		it 'returns docked bikes' do
+			bike = Bike.new #basically the same as if we ran release bike first
+    		subject.dock(bike)
 			expect(subject.bike).to eq bike
 		end
 
 		it { is_expected.to respond_to(:bike)}
 
-  	it { is_expected.to respond_to(:dock).with(1).argument }
+  		it { is_expected.to respond_to(:dock).with(1).argument }
+  	end
 end
