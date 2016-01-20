52442c49a8343ef633ed316996d2386466b946eb
ZeshanRasul <zeshanrasul90@gmail.com>
2016-01-20 17:19:13 +0000

    Added private empty and full methods

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -9,15 +9,25 @@ class DockingStation
 	end
 
 	def release_bike
-		fail "No Bike Error" unless @bike
+		fail "No Bike Error" if empty?
 		@bikes
 		@bike
 	end
 
 	def dock(bike)
-		raise "Dock Full" if @bikes.length >= 20
+		raise "Dock Full" if full?
 		@bike = bike
 		@bikes << bike
 		@bike
 	end
+
+	private
+	def full?
+		bikes.count >= 20? true : false
+	end
+
+	def empty?
+		bikes.count <= 0? true : false
+	end
+
 end
