be1d9542763cb42a3fb2b58f7730b668ad4958e1
innlouvate <l.j.franklin85@googlemail.com>
2016-01-21 12:10:41 +0000

    ex13 - incl doubles in spec

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -7,8 +7,7 @@ describe DockingStation do
 
 		describe '#release_bike' do
 			it 'releases a bike' do
-				bike = Bike.new
-				subject.dock(bike)
+				subject.dock(double(:bike))
 	  		expect(subject.release_bike).to eq bike
 	  	end
 		end
@@ -21,52 +20,47 @@ describe DockingStation do
 
     describe '#dock' do
     	it 'returns docked bikes' do
-    		bike = Bike.new
-    		subject.dock(bike)
+    		subject.dock double(:bike)
     		expect(subject.bikes).to include bike
     	end
     end
 
     describe '#dock' do
       it 'expects dock to raise an error' do
-      subject.capacity.times { subject.dock Bike.new }
-        expect { subject.dock(Bike.new) }.to raise_error("The dock is full")
+      subject.capacity.times { subject.dock double(:bike) }
+        expect { subject.dock double(:test_bike) }.to raise_error("The dock is full")
       end
     end
 
   	it { is_expected.to respond_to(:dock).with(2).argument }
 
-		it 'has a default capacity' do
-			expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-		end
 		describe 'initialization' do
 			subject { DockingStation.new }
 			it 'defaults capacity' do
-				DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
-				expect{ subject.dock(Bike.new) }.to raise_error("The dock is full")
+				DockingStation::DEFAULT_CAPACITY.times { subject.dock double(:bike) }
+				expect{ subject.dock double(:test_bike) }.to raise_error("The dock is full")
 			end
 		end
 
 		#marked/reports bike as not working
 		describe '#dock' do
 			it 'reports bike as not working when you dock it' do
-				bike = Bike.new
-				expect{subject.dock(bike, false)}.to change{ bike.working? }.from(true).to(false)
+				expect{subject.dock(double(:bike), false)}.to change{ bike.working? }.from(true).to(false)
 			end
 		end
 
 		describe '#dock' do
 			it 'expects any bike to be docked, broken or not' do
-				subject.dock(Bike.new)
-				subject.dock(Bike.new, false)
+				subject.dock(double(:bike))
+				subject.dock(double(:broken_bike), false)
 				expect(subject.bikes.length).to eq 2
 			end
 		end
 
 		describe '#release_bike' do
 			it "releases working bikes only" do
-				subject.dock(Bike.new)
-				subject.dock(Bike.new, false)
+				subject.dock(double(:bike))
+				subject.dock(double(:broken_bike), false)
 				bike = subject.release_bike
 				expect(bike).to be_working
 			end
