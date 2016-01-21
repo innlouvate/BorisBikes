186ef48782b4696526ae3d25ab2fcb8b7a4a67e6
innlouvate <l.j.franklin85@googlemail.com>
2016-01-21 14:20:11 +0000

    rspec tests fixed stubs

--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -1,15 +1,14 @@
 require 'Bike'
 
 describe Bike do
-	subject(:Bike) {described_class.new}
-	let(:station) { double :station }
 
 	it { is_expected.to respond_to :working? }
 
-	describe '#toggle_working' do
-		it 'reports bike as not working when you dock it' do
-			allow(station).to receive(:dock)
-			expect{bike}.to change{ subject.working? }.to(false)
+	describe '#working?' do
+		it 'toggle changes working status' do
+			bike = Bike.new
+			bike.toggle_working
+			expect(bike).to_not be_working
 		end
 	end
 
