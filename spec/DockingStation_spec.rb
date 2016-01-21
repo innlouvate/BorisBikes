39dd3f58044e3fea6159554f434492726b11dc1b
ZeshanRasul <zeshanrasul90@gmail.com>
2016-01-21 16:07:08 +0000

    Used doubles in DockingStation_spec.rb

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -13,20 +13,30 @@ describe DockingStation do
   		end
 
 		describe "#release_bike" do
+			# let(:bike) { double :bike }
 			it "releases a bike" do
-				bike = Bike.new
+				# bike = Bike.new
+				bike = double("bike",:broken? => false)
 				subject.dock(bike)
 				expect(subject.release_bike).to eq bike
 			end
 
 			it 'expects bike to be working in release_bike' do
-				bike = Bike.new
+				# bike = Bike.new
+				bike = double("bike", :broken? => nil)
 				expect(bike.broken?).to be nil
 			end
-
+			# let(:docking_station) { double :DockingStation }
 			it "Does not release the bike if the bike is broken" do
-				bike = Bike.new
+				# bike = Bike.new
+				bike = double("bike", :report_broken => true, :broken? => true)
+				# allow(bike).to receive(:report_broken).and_return(true)
+				# allow(bike).to receive(:broken?).and_return(true)
 				bike.report_broken
+
+				# allow(bike).to receive(:broken).and_return(true)
+				# allow(docking_station).to receive(:release_bike).and_return{ fail "Bike is broken!" }
+				# allow(docking_station).to receive(:dock).and_return(bike)
 				subject.dock(bike)
 				expect{subject.release_bike}.to raise_error "Bike is broken!"
 			end
@@ -40,18 +50,24 @@ describe DockingStation do
 		end
 
 		describe "#dock_bike" do
-			bike = Bike.new #basically the same as if we ran release bike first
+			# let(:bike) { double :bike }
+			# bike = Bike.new #basically the same as if we ran release bike first
 			it 'returns docked bike' do
+				bike = double("bike")
 				expect(subject.dock(bike)).to eq bike
 			end
 
 			it 'docks broken bikes' do
-				bike = Bike.new
+				# bike = Bike.new
+				bike = double("bike", :report_broken => true)
+				# allow(bike).to receive(:report_broken).and_return(true)
 				bike.report_broken
 				expect(subject.dock(bike)).to eq bike
 			end
 
 			it 'should raise error if dock is full' do
+				bike = double("bike") #:report_broken => true)
+
 				subject.capacity.times do
 					 subject.dock(bike)
 				end
