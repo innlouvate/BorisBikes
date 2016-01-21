52fc3231df302f71446c632f0afd8f673a0bd805
ZeshanRasul <zeshanrasul90@gmail.com>
2016-01-21 15:35:07 +0000

    Docks broken bike test

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -30,6 +30,7 @@ describe DockingStation do
 				subject.dock(bike)
 				expect{subject.release_bike}.to raise_error "Bike is broken!"
 			end
+
 		end
 
 		describe "release bike failure" do
@@ -41,7 +42,12 @@ describe DockingStation do
 		describe "#dock_bike" do
 			bike = Bike.new #basically the same as if we ran release bike first
 			it 'returns docked bike' do
-	    		# subject.dock(bike)
+				expect(subject.dock(bike)).to eq bike
+			end
+
+			it 'docks broken bikes' do
+				bike = Bike.new
+				bike.report_broken
 				expect(subject.dock(bike)).to eq bike
 			end
 
